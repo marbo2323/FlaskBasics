3af7ce2d8f9502f2990366171965261d66647d5f
Arbo Lindma <arbo.lindma@gmail.com>
2020-02-22 22:35:46 +0200

    Read request parameter

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello from Treehouse"
+def index(name="Treehouse"):
+    name = request.args.get("name", name)
+    return "Hello from {}".format(name)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
