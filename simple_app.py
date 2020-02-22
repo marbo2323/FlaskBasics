bff6eea499c7e4eeab5a8752a6ec4dabcb2c954a
Arbo Lindma <arbo.lindma@gmail.com>
2020-02-23 00:05:07 +0200

    Pass variable to template as dictionary

--- simple_app.py
+++ simple_app.py
@@ -15,7 +15,8 @@ def index(name="Treehouse"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-    return render_template("add.html", num1=num1, num2=num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 
 @app.route('/multiply')
