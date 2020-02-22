e382dc3c7b8ddad82dcfeef03cf74d131dc833cf
Arbo Lindma <arbo.lindma@gmail.com>
2020-02-22 22:59:08 +0200

    Flask type coversion

--- simple_app.py
+++ simple_app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index(name="Treehouse"):
     return "Hello from {}".format(name)
 
-@app.route('/add/<num1>/<num2>')
+@app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
-    return '{0} + {1} = {2}'.format(num1, num2, int(num1) + int(num2))
+    return '{0} + {1} = {2}'.format(num1, num2, num1 + num2)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
